f76cac902985decb099019faee6e48ee36182a5d
omkarbdesai <omkarbdesai@gmail.com>
2020-11-16 21:41:27 -0500

    adding some workload characterization for read write ratio

--- util/workload_characterizer.cc
+++ util/workload_characterizer.cc
@@ -8,4 +8,33 @@ WorkkloadType::WorkkloadType() {
   skew = 0.0;
 }
 
+void WorkkloadType::getCountInc() {
+  if (windowCount == 10000){
+    windowCount = 0;
+    getCount = 0;
+  }
+  windowCount++;
+  getCount++;
+  readWriteRatio = getCount/putCount;
+}
+
+void WorkkloadType::putCountInc() {
+  if (windowCount == 10000){
+    windowCount = 0;
+    putCount = 0;
+  }
+  windowCount++;
+  putCount++;
+  readWriteRatio = getCount/putCount;
+}
+
+double WorkkloadType::getOverlapRatio() {
+  if(readWriteRatio == 0){
+    return 0.0;
+  }
+  else{
+    return 0.4;
+  }
+}
+
 }
